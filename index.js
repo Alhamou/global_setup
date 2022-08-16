f7e20f13926e673488326a06c4a79843ebcc31ae
Emad Alhamou <emadalhamo@gmail.com>
2022-08-16 12:35:39 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
 let obj = {}
 
-addMudole = name => obj = {...obj, ...require(name)}
 
 const modules = ["./func1","./func2", "./out.js"]
 
-modules.forEach(addMudole)
+modules.forEach(name => obj = {...obj, ...require(name)})
 
 for(let attr in obj)
     global[attr] = obj[attr]
