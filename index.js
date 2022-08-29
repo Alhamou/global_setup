b07964d8bd40a72d9bbd8ea370c524e5ff995e90
Emad Alhamou <emadalhamo@gmail.com>
2022-08-29 16:44:14 +0200

    dev

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const modules = ["./func1.js","./func2.js", "./out.js"]
+
 modules.forEach(name => {
     let obj = {...require(name)}
     for(let key in obj) global[key] = obj[key]
 })
-
 
 global.resolveGlobals = async ()=>{
     for(let attr in global){
